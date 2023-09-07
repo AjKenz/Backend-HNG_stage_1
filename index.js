c26eb191daad38ab699491d9c3002e2b39127550
KenNY <ajibskenny@gmail.com>
2023-09-07 20:57:29 +0100

    completed stage 1

--- index.js
+++ index.js
@@ -6,26 +6,37 @@ const app = express()
 app.use(express.json())
 
 app.get('/api', async (req, res) => {
-    try{
-        const {slack_name, track} = req.query
+    try {
+        const { slack_name, track } = req.query
+        const currentDate = new Date();
+        const currentDay = currentDate.toLocaleDateString('en-US', { weekday: 'long' })
 
-        res.json({
+
+        const currentTime = new Date();
+        const randomOffset = (Math.random() * 4 - 2) * 60 * 1000;
+        const utcTimeWithOffset = new Date(currentTime.getTime() + randomOffset);
+        const formattedUtcTime = utcTimeWithOffset.toISOString().slice(0, -5) + 'Z';
+
+        res.status(200).json({
             slack_name: slack_name,
-            current_day: '',
-            utc_time: '',
+            current_day: currentDay,
+            utc_time: formattedUtcTime,
             track: track,
-            git_file_url: '',
-            git_repo_url: '',
-            status_code: 'success'
+            github_file_url: 'https://github.com/AjKenz/HNG_stage_1/blob/master/index.js',
+            github_repo_url: 'https://github.com/AjKenz/HNG_stage_1',
+            status_code: 200
         })
     }
-    catch(error){
+    catch (error) {
         console.log('an error occured ' + error.message)
+        res.status(500).json({
+            message: 'Internal server Error' || error.message
+        })
     }
 })
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.json({
         message: 'HNG backend stage 1'
     })
@@ -34,5 +45,5 @@ app.get('/', (req, res)=>{
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
-    console.log('Server is runnig on: '+ port)
+    console.log('Server is runnig on: ' + port)
 })
